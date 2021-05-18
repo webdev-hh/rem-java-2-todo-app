b97f6ff74219bfff343dcf983e9dec1d3e50b282
Jan Robert <janmartinrobert@gmail.com>
2021-05-18 14:25:50 +0200

    Load todos from server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,20 @@ import Page from './components/Page'
 import Header from './components/Header'
 import AddATodo from './components/AddATodo'
 import Main from './components/Main'
-
-const todos = [
-  {
-    id: '976f37f4-8aa7-481d-ad2c-2120613f3347',
-    description: 'Write tests',
-    status: 'OPEN',
-  },
-  {
-    id: '61084198-b1b7-4d7c-837c-62f458ce765a',
-    description: 'Drink coffee',
-    status: 'IN_PROGRESS',
-  },
-  {
-    id: '4f5cf145-d5f7-430f-8e0e-048ea3c1fc68',
-    description: 'Buy milk',
-    status: 'DONE',
-  },
-]
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('/api/todo')
+      .then(response => response.data)
+      .then(todos => setTodos(todos))
+      .catch(error => console.error(error))
+  }, [])
+
   return (
     <Page>
       <Header />
