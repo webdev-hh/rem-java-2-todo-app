4612c1ece212e57f937a3dacbd140efdd1c32056
Jan Robert <janmartinrobert@gmail.com>
2021-05-18 13:09:43 +0200

    Add Main and Board

---
+++ frontend/src/components/Board.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components/macro'
+
+export default function Board({ title }) {
+  return (
+    <Wrapper>
+      <summary>
+        <h2>{title}</h2>
+      </summary>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        aliquam amet aperiam commodi dolorum eum id illo ipsa libero molestias,
+        nostrum obcaecati quo saepe sapiente tempore ullam, vel. Ducimus, sint.
+      </p>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.details`
+  h2 {
+    display: inline-block;
+  }
+`
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,39 +1,16 @@
+import Board from './Board'
+import styled from 'styled-components/macro'
+
 export default function Main() {
   return (
-    <main>
-      <details>
-        <summary>
-          <h2>Todo</h2>
-        </summary>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aliquam amet aperiam commodi dolorum eum id illo ipsa libero
-          molestias, nostrum obcaecati quo saepe sapiente tempore ullam, vel.
-          Ducimus, sint.
-        </p>
-      </details>
-      <details>
-        <summary>
-          <h2>Doing</h2>
-        </summary>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aliquam amet aperiam commodi dolorum eum id illo ipsa libero
-          molestias, nostrum obcaecati quo saepe sapiente tempore ullam, vel.
-          Ducimus, sint.
-        </p>
-      </details>
-      <details>
-        <summary>
-          <h2>Done</h2>
-        </summary>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          aliquam amet aperiam commodi dolorum eum id illo ipsa libero
-          molestias, nostrum obcaecati quo saepe sapiente tempore ullam, vel.
-          Ducimus, sint.
-        </p>
-      </details>
-    </main>
+    <Wrapper>
+      <Board title="Todo" />
+      <Board title="Doing" />
+      <Board title="Done" />
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+  padding: 24px;
+`
